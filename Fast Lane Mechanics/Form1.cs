5e0468ca978912c8620c4a08b76402379bfd671d
DESKTOP-C1IDNB2\Dino <wasiqtayyab30@gmail.com>
2022-05-01 00:59:36 +0500

    commit all project files

---
+++ Fast Lane Mechanics/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fast_Lane_Mechanics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
